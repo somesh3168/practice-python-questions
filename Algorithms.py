76fd404c02dd9aaed4d273726115e078d91b8e03
Somesh Samadder <42448930+somesh3168@users.noreply.github.com>
2021-04-21 11:35:32 +0530

    Linear Search Algo

--- Algorithms.py
+++ Algorithms.py
@@ -12,3 +12,16 @@ while les[f] != res:
 print(count,f)
 
 #######################################
+####### Linear Search Algo
+les = [1, 3, 7, 9, 14, 19, 45]
+res = 14
+# linear search
+count = 0
+if res in les:
+    for i,v in enumerate(les):
+        if v == res:
+            count = i+1
+            globals()  ['f'] = i
+    
+print(count,f)
+
