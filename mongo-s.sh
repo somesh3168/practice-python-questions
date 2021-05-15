898f4454e3a7717aee88d78e192705426dd928fc
Somesh Samadder <42448930+somesh3168@users.noreply.github.com>
2021-05-15 13:24:17 +0530

    Create mongo-s.sh

---
+++ mongo-s.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+#source:= https://www.scaleway.com/en/docs/install-and-secure-mongodb-on-ubuntu/
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A14518585931BC711F9BA15703C6
+sudo apt-get update && sudo apt-get upgrade -y
+
+sudo apt-get install mongodb-org
+sudo systemctl start mongod
+or
+sudo service mongod status/start
+
+# 4 . Ensure that it restarts automatically at each boot
+
+sudo systemctl enable mongod
